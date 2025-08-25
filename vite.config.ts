cee105a0e42938169d84d6eb5974a46c92161809
sjoek <sjoek@example.com>
2025-08-24 22:00:05 -0700

    Configure for main branch deployment - Update base path and include dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/patient-chart-system/',
+  base: '/qiholeinone-new-patient-chart/',
   plugins: [react()],
 })
